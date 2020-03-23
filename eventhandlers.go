d943d281f21a9e1f0c494a363cb2db0840671860
Grabner <andreas.grabner@dynatrace.com>
2020-03-24 00:14:10 +0100

    added initial support for executing http requests

--- eventhandlers.go
+++ eventhandlers.go
@@ -3,39 +3,74 @@ package main
 import (
 	"fmt"
 
-	keptnutils "github.com/keptn/go-utils/pkg/utils"
 	keptnevents "github.com/keptn/go-utils/pkg/events"
+	keptnutils "github.com/keptn/go-utils/pkg/utils"
 
 	"github.com/cloudevents/sdk-go/pkg/cloudevents"
 )
 
 /**
- * Here are all the handler functions for the individual events
-   -> "sh.keptn.event.configuration.change"
-   -> "sh.keptn.events.deployment-finished"
-   -> "sh.keptn.events.tests-finished"
-   -> "sh.keptn.event.start-evaluation"
-   -> "sh.keptn.events.evaluation-done"
-   -> "sh.keptn.event.problem.open"
-   -> "sh.keptn.events.problem"
- */
+* Here are all the handler functions for the individual events
+  -> "sh.keptn.event.configuration.change"
+  -> "sh.keptn.events.deployment-finished"
+  -> "sh.keptn.events.tests-finished"
+  -> "sh.keptn.event.start-evaluation"
+  -> "sh.keptn.events.evaluation-done"
+  -> "sh.keptn.event.problem.open"
+  -> "sh.keptn.events.problem"
+*/
+
+//
+// if the passed files exist either executes the bash or the http request
+//
+func executeScriptOrHTTP(data *keptnevents.ConfigurationChangeEventData, logger *keptnutils.Logger, bashFilename, httpFilename string) (bool, error) {
+	resource, err := getKeptnResource(data.Project, data.Service, data.Stage, bashFilename, logger)
+	if resource != "" && err == nil {
+		logger.Info("Found script " + bashFilename)
+		_, err = executeCommand(resource, nil, logger)
+		if err != nil {
+			return false, err
+		}
+	} else {
+		logger.Info("No script found at " + bashFilename)
+	}
+
+	var parsedRequest genericHttpRequest
+	resource, err = getKeptnResource(data.Project, data.Service, data.Stage, httpFilename, logger)
+	if resource != "" && err == nil {
+		logger.Info("Found http request " + httpFilename)
+		parsedRequest, err = parseHttpRequestFromHttpTextFile(httpFilename)
+		if err == nil {
+			statusCode, body, requestError := executeGenericHttpRequest(parsedRequest)
+			if requestError != nil {
+				logger.Error(fmt.Sprintf("Error: %s", err.Error()))
+				return false, err
+			} else {
+				logger.Info(fmt.Sprintf("%d - %s", statusCode, body))
+			}
+		} else {
+			return false, err
+		}
+	} else {
+		logger.Info("No http found at " + httpFilename)
+	}
+
+	return true, nil
+}
 
 //
 // Handles ConfigurationChangeEventType = "sh.keptn.event.configuration.change"
 // TODO: add in your handler code
 //
 func handleConfigurationChangeEvent(event cloudevents.Event, shkeptncontext string, data *keptnevents.ConfigurationChangeEventData, logger *keptnutils.Logger) error {
-	logger.Info(fmt.Sprintf("Handling Configuration Changed Event: %s", event.Context.GetID()));
+	logger.Info(fmt.Sprintf("Handling Configuration Changed Event: %s", event.Context.GetID()))
 
-	resource, err := getKeptnResource(data.Project, data.Service, data.Stage, "./scripts/configuration.change.sh", logger)
-	if (resource != "" && err == nil) {
-		logger.Info("Found script ./scripts/configuration.change.sh");
-		executeCommand(resource, nil, logger);
-	} else {
-		logger.Info("No script found at ./scripts/configuration.change.sh")
+	_, err := executeScriptOrHTTP(data, logger, "./scripts/configuration.change.sh", "./scripts/configuration.change.http")
+	if err != nil {
+		logger.Error(fmt.Sprintf("Error: %s", err.Error()))
 	}
 
-	return nil
+	return err
 }
 
 //
@@ -43,7 +78,7 @@ func handleConfigurationChangeEvent(event cloudevents.Event, shkeptncontext stri
 // TODO: add in your handler code
 //
 func handleDeploymentFinishedEvent(event cloudevents.Event, shkeptncontext string, data *keptnevents.DeploymentFinishedEventData, logger *keptnutils.Logger) error {
-	logger.Info(fmt.Sprintf("Handling Deployment Finished Event: %s", event.Context.GetID()));
+	logger.Info(fmt.Sprintf("Handling Deployment Finished Event: %s", event.Context.GetID()))
 
 	return nil
 }
@@ -53,7 +88,7 @@ func handleDeploymentFinishedEvent(event cloudevents.Event, shkeptncontext strin
 // TODO: add in your handler code
 //
 func handleTestsFinishedEvent(event cloudevents.Event, shkeptncontext string, data *keptnevents.TestsFinishedEventData, logger *keptnutils.Logger) error {
-	logger.Info(fmt.Sprintf("Handling Tests Finished Event: %s", event.Context.GetID()));
+	logger.Info(fmt.Sprintf("Handling Tests Finished Event: %s", event.Context.GetID()))
 
 	return nil
 }
@@ -63,7 +98,7 @@ func handleTestsFinishedEvent(event cloudevents.Event, shkeptncontext string, da
 // TODO: add in your handler code
 //
 func handleStartEvaluationEvent(event cloudevents.Event, shkeptncontext string, data *keptnevents.StartEvaluationEventData, logger *keptnutils.Logger) error {
-	logger.Info(fmt.Sprintf("Handling Start Evaluation Event: %s", event.Context.GetID()));
+	logger.Info(fmt.Sprintf("Handling Start Evaluation Event: %s", event.Context.GetID()))
 
 	return nil
 }
@@ -73,7 +108,7 @@ func handleStartEvaluationEvent(event cloudevents.Event, shkeptncontext string, 
 // TODO: add in your handler code
 //
 func handleEvaluationDoneEvent(event cloudevents.Event, shkeptncontext string, data *keptnevents.EvaluationDoneEventData, logger *keptnutils.Logger) error {
-	logger.Info(fmt.Sprintf("Handling Evaluation Done Event: %s", event.Context.GetID()));
+	logger.Info(fmt.Sprintf("Handling Evaluation Done Event: %s", event.Context.GetID()))
 
 	return nil
 }
@@ -84,7 +119,7 @@ func handleEvaluationDoneEvent(event cloudevents.Event, shkeptncontext string, d
 // TODO: add in your handler code
 //
 func handleProblemEvent(event cloudevents.Event, shkeptncontext string, data *keptnevents.ProblemEventData, logger *keptnutils.Logger) error {
-	logger.Info(fmt.Sprintf("Handling Problem Event: %s", event.Context.GetID()));
+	logger.Info(fmt.Sprintf("Handling Problem Event: %s", event.Context.GetID()))
 
 	return nil
 }
--- helpers.go
+++ helpers.go
@@ -1,31 +1,34 @@
 package main
 
 import (
+	"bytes"
 	"context"
 	"errors"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
 	"net/url"
 	"os"
 	"strings"
 	"time"
-	"log"
 
 	"github.com/cloudevents/sdk-go/pkg/cloudevents"
 	"github.com/cloudevents/sdk-go/pkg/cloudevents/client"
 	cloudeventshttp "github.com/cloudevents/sdk-go/pkg/cloudevents/transport/http"
-	"github.com/google/uuid"
 	"github.com/cloudevents/sdk-go/pkg/cloudevents/types"
+	"github.com/google/uuid"
 
 	configutils "github.com/keptn/go-utils/pkg/configuration-service/utils"
+	keptnevents "github.com/keptn/go-utils/pkg/events"
 	keptnutils "github.com/keptn/go-utils/pkg/utils"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	keptnevents "github.com/keptn/go-utils/pkg/events"
 )
 
 /**
  * Helper Functions
  */
- func getConfigurationServiceURL() string {
+func getConfigurationServiceURL() string {
 	if os.Getenv("env") == "production" {
 		return "configuration-service:8080"
 	}
@@ -38,9 +41,9 @@ import (
 func getKeptnResource(project, stage, service, resource string, logger *keptnutils.Logger) (string, error) {
 
 	// if we run in a runlocal mode we are just getting the file from the local disk
-	if(runlocal) {
-		return _getKeptnResourceFromLocal(project, stage, service, resource, logger);
-	} 
+	if runlocal {
+		return _getKeptnResourceFromLocal(project, stage, service, resource, logger)
+	}
 
 	// get it from keptn
 	resourceHandler := configutils.NewResourceHandler(getConfigurationServiceURL())
@@ -84,12 +87,12 @@ func getKeptnResource(project, stage, service, resource string, logger *keptnuti
 /**
  * Retrieves a resource (=file) from the local file system. Basically checks if the file is available and if so returns it
  */
-func _getKeptnResourceFromLocal(project string, stage string, service string, resource string, logger *keptnutils.Logger) (string, error) {
+func _getKeptnResourceFromLocal(project, stage, service, resource string, logger *keptnutils.Logger) (string, error) {
 	if _, err := os.Stat(resource); err == nil {
-		return resource, nil  
+		return resource, nil
 	} else {
 		return "", err
-	}	
+	}
 }
 
 /**
@@ -111,18 +114,191 @@ func getKeptnDomain() (string, error) {
 }
 
 //
-// Executes a command, e.g: ls -l
+// replaces $ placeholders with actual values
+// $PROJECT, $STAGE, $SERVICE, $DEPLOYMENT
+// $TESTSTRATEGY
+// $LABEL.XXXX  -> will replace that with a label called XXXX
+// $ENV.XXXX    -> will replace that with an env variable called XXXX
+// $SECRET.YYYY -> will replace that with the k8s secret called YYYY
+//
+func replaceKeptnPlaceholders(input string, project, stage, service, deployment, teststrategy string, labels map[string]string) string {
+	result := input
+
+	// first we do the regular keptn values
+	result = strings.Replace(result, "$PROJECT", project, -1)
+	result = strings.Replace(result, "$STAGE", stage, -1)
+	result = strings.Replace(result, "$SERVICE", service, -1)
+	result = strings.Replace(result, "$DEPLOYMENT", deployment, -1)
+	result = strings.Replace(result, "$TESTSTRATEGY", teststrategy, -1)
+
+	// now we do the labels
+	for key, value := range labels {
+		result = strings.Replace(result, "$LABEL."+key, value, -1)
+	}
+
+	// now we do all environment variables
+	for _, env := range os.Environ() {
+		pair := strings.SplitN(env, "=", 2)
+		result = strings.Replace(result, "$ENV."+pair[0], pair[1], -1)
+	}
+
+	// TODO: iterate through k8s secrets!
+
+	return result
+}
+
+func _nextCleanLine(lines []string, lineIx int, trim bool) (int, string) {
+	// sanity check
+	lineIx++
+	maxLines := len(lines)
+	if lineIx < 0 || maxLines <= 0 || lineIx >= maxLines {
+		return -1, ""
+	}
+
+	line := ""
+	for ; lineIx < maxLines; lineIx++ {
+		line = lines[lineIx]
+		if trim {
+			line = strings.Trim(line, " ")
+		}
+		if strings.HasPrefix(line, "#") {
+			continue
+		}
+
+		// stop if we found a new line that is not a comment!
+		if len(line) >= 0 {
+			break
+		}
+	}
+
+	// have we reached the end of the strings?
+	if lineIx >= maxLines {
+		return lineIx, ""
+	}
+	return lineIx, line
+}
+
+type genericHttpRequest struct {
+	method  string
+	uri     string
+	headers map[string]string
+	body    string
+}
+
+//
+// Parses .http raw file content and returns HTTP METHOD, URI, HEADERS, BODY
+//
+func parseHttpRequestFromHttpTextFile(httpfile string) (genericHttpRequest, error) {
+	var returnRequest genericHttpRequest
+
+	content, err := ioutil.ReadFile(httpfile)
+	if err != nil {
+		return returnRequest, err
+	}
+
+	return parseHttpRequestFromString(string(content))
+}
+
+//
+// Parses .http string content and returns HTTP METHOD, URI, HEADERS, BODY
+//
+func parseHttpRequestFromString(rawContent string) (genericHttpRequest, error) {
+	var returnRequest genericHttpRequest
+
+	// lets get each line
+	lines := strings.Split(rawContent, "\n")
+
+	//
+	// lets find the first clean line - must be the HTTP Method and URI, e.g: GET http://myuri
+	lineIx, line := _nextCleanLine(lines, -1, true)
+	if lineIx < 0 {
+		return returnRequest, errors.New("No HTTP Method or URI Found")
+	}
+
+	lineSplits := strings.Split(line, " ")
+	if len(lineSplits) == 1 {
+		// only provides URI
+		returnRequest.uri = lineSplits[0]
+	} else {
+		// provides method and URI
+		returnRequest.method = lineSplits[0]
+		returnRequest.uri = lineSplits[1]
+	}
+
+	//
+	// now lets iterate through the next lines as they should all be headers until we end up with a blank line or EOF
+	returnRequest.headers = make(map[string]string)
+	lineIx, line = _nextCleanLine(lines, lineIx, true)
+	for (lineIx > 0) && (len(line) > 0) {
+		lineSplits = strings.Split(line, ":")
+		if len(lineSplits) < 2 {
+			break
+		}
+		headerName := strings.Trim(lineSplits[0], " ")
+		headerValue := strings.Trim(lineSplits[1], " ")
+		returnRequest.headers[headerName] = headerValue
+		lineIx, line = _nextCleanLine(lines, lineIx, true)
+	}
+
+	//
+	// if we still have content it must be the request body
+	returnRequest.body = ""
+	lineIx, line = _nextCleanLine(lines, lineIx, false)
+	for lineIx > 0 && len(line) > 0 {
+		returnRequest.body += line
+		returnRequest.body += "\n"
+		lineIx, line = _nextCleanLine(lines, lineIx, false)
+	}
+
+	return returnRequest, nil
+}
+
+//
+// Sends a generic HTTP Request
+//
+func executeGenericHttpRequest(request genericHttpRequest) (int, string, error) {
+	client := http.Client{}
+
+	// define the request
+	log.Println(request.method, request.uri, request.uri, request.body)
+	req, err := http.NewRequest(request.method, request.uri, bytes.NewBufferString(request.body))
+
+	if err != nil {
+		log.Println(err.Error())
+		return -1, "", err
+	}
+
+	// add the headers
+	for key, value := range request.headers {
+		req.Header.Add(key, value)
+	}
+
+	// execute
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Println(err.Error())
+		return -1, "", err
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+
+	return resp.StatusCode, string(body), err
+}
+
+//
+// Executes a command, e.g: ls -l; ./yourscript.sh
 //
 func executeCommand(command string, args []string, logger *keptnutils.Logger) (bool, error) {
 	res, err := keptnutils.ExecuteCommand(command, args)
-	
+
 	logger.Info(res)
 	if err != nil {
 		logger.Error(err.Error())
 		return false, err
 	}
 
-	logger.Debug("Successfull executed command: " + command);
+	logger.Debug("Successfull executed command: " + command)
 
 	return true, nil
 }
@@ -135,16 +311,24 @@ func sendConfigurationChangeEvent(shkeptncontext string, incomingEvent *cloudeve
 	contentType := "application/json"
 
 	configurationChangeData := keptnevents.ConfigurationChangeEventData{}
-	
+
 	// if we have an incoming event we pre-populate data
 	if incomingEvent != nil {
 		incomingEvent.DataAs(&configurationChangeData)
 	}
 
-	if project != "" 	{ configurationChangeData.Project = project }
-	if service != "" 	{ configurationChangeData.Service = service }
-	if stage != "" 		{ configurationChangeData.Stage = stage }
-	if labels != nil 	{ configurationChangeData.Labels = labels }
+	if project != "" {
+		configurationChangeData.Project = project
+	}
+	if service != "" {
+		configurationChangeData.Service = service
+	}
+	if stage != "" {
+		configurationChangeData.Stage = stage
+	}
+	if labels != nil {
+		configurationChangeData.Labels = labels
+	}
 
 	event := cloudevents.Event{
 		Context: cloudevents.EventContextV02{
@@ -158,10 +342,10 @@ func sendConfigurationChangeEvent(shkeptncontext string, incomingEvent *cloudeve
 		Data: configurationChangeData,
 	}
 
-	if (logger != nil) {
+	if logger != nil {
 		logger.Info(fmt.Sprintf("%s", event))
 	} else {
-		log.Println(fmt.Sprintf("%s", event));
+		log.Println(fmt.Sprintf("%s", event))
 	}
 
 	return sendCloudNativeEvent(event)
@@ -170,26 +354,42 @@ func sendConfigurationChangeEvent(shkeptncontext string, incomingEvent *cloudeve
 //
 // Sends a DeploymentFinishedEventType = "sh.keptn.events.deployment-finished"
 //
- func sendDeploymentFinishedEvent(shkeptncontext string, incomingEvent *cloudevents.Event, project, service, stage, teststrategy, deploymentstrategy, image, tag, deploymentURILocal, deploymentURIPublic string, labels map[string]string, logger *keptnutils.Logger) error {
+func sendDeploymentFinishedEvent(shkeptncontext string, incomingEvent *cloudevents.Event, project, service, stage, teststrategy, deploymentstrategy, image, tag, deploymentURILocal, deploymentURIPublic string, labels map[string]string, logger *keptnutils.Logger) error {
 	source, _ := url.Parse("jenkins-service")
 	contentType := "application/json"
 
 	deploymentFinishedData := keptnevents.DeploymentFinishedEventData{}
-	
+
 	// if we have an incoming event we pre-populate data
 	if incomingEvent != nil {
 		incomingEvent.DataAs(&deploymentFinishedData)
 	}
 
-	if project != "" 			{ deploymentFinishedData.Project = project }
-	if service != "" 			{ deploymentFinishedData.Service = service	}
-	if stage != ""   			{ deploymentFinishedData.Stage = stage	}
-	if teststrategy != "" 		{ deploymentFinishedData.TestStrategy = teststrategy }
-	if deploymentstrategy != "" { deploymentFinishedData.DeploymentStrategy = deploymentstrategy }
-	if image != ""   			{ deploymentFinishedData.Image = image }
-	if tag != ""   				{ deploymentFinishedData.Tag = tag }
-	
-	if labels != nil 			{ deploymentFinishedData.Labels = labels }
+	if project != "" {
+		deploymentFinishedData.Project = project
+	}
+	if service != "" {
+		deploymentFinishedData.Service = service
+	}
+	if stage != "" {
+		deploymentFinishedData.Stage = stage
+	}
+	if teststrategy != "" {
+		deploymentFinishedData.TestStrategy = teststrategy
+	}
+	if deploymentstrategy != "" {
+		deploymentFinishedData.DeploymentStrategy = deploymentstrategy
+	}
+	if image != "" {
+		deploymentFinishedData.Image = image
+	}
+	if tag != "" {
+		deploymentFinishedData.Tag = tag
+	}
+
+	if labels != nil {
+		deploymentFinishedData.Labels = labels
+	}
 
 	event := cloudevents.Event{
 		Context: cloudevents.EventContextV02{
@@ -203,10 +403,10 @@ func sendConfigurationChangeEvent(shkeptncontext string, incomingEvent *cloudeve
 		Data: deploymentFinishedData,
 	}
 
-	if (logger != nil) {
+	if logger != nil {
 		logger.Info(fmt.Sprintf("%s", event))
 	} else {
-		log.Println(fmt.Sprintf("%s", event));
+		log.Println(fmt.Sprintf("%s", event))
 	}
 
 	return sendCloudNativeEvent(event)
@@ -227,13 +427,25 @@ func sendTestsFinishedEvent(shkeptncontext string, incomingEvent *cloudevents.Ev
 		incomingEvent.DataAs(&testFinishedData)
 	}
 
-	if project != "" 			{ testFinishedData.Project = project }
-	if service != "" 			{ testFinishedData.Service = service	}
-	if stage != ""   			{ testFinishedData.Stage = stage	}
-	if teststrategy != "" 		{ testFinishedData.TestStrategy = teststrategy }
-	if deploymentstrategy != "" { testFinishedData.DeploymentStrategy = deploymentstrategy }
-	
-	if labels != nil 			{ testFinishedData.Labels = labels }
+	if project != "" {
+		testFinishedData.Project = project
+	}
+	if service != "" {
+		testFinishedData.Service = service
+	}
+	if stage != "" {
+		testFinishedData.Stage = stage
+	}
+	if teststrategy != "" {
+		testFinishedData.TestStrategy = teststrategy
+	}
+	if deploymentstrategy != "" {
+		testFinishedData.DeploymentStrategy = deploymentstrategy
+	}
+
+	if labels != nil {
+		testFinishedData.Labels = labels
+	}
 
 	// fill in timestamps
 	testFinishedData.Start = startedAt.Format(time.RFC3339)
@@ -254,10 +466,10 @@ func sendTestsFinishedEvent(shkeptncontext string, incomingEvent *cloudevents.Ev
 		Data: testFinishedData,
 	}
 
-	if (logger != nil) {
+	if logger != nil {
 		logger.Info(fmt.Sprintf("%s", event))
 	} else {
-		log.Println(fmt.Sprintf("%s", event));
+		log.Println(fmt.Sprintf("%s", event))
 	}
 
 	return sendCloudNativeEvent(event)
